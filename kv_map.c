cc9463814a547043739a6850ce63478e766235d7
Martin Polasek <polasek.martin@seznam.cz>
2021-09-24 00:54:43 +0200

    First

---
+++ kv_map.c
@@ -0,0 +1,424 @@
+/*
+ * config.c
+ *
+ *  Created on: 6. 5. 2021
+ *      Author: polasekm
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdlib.h> //atof, atoi
+
+#include "kv_map.h"
+
+//------------------------------------------------------------------------------
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+
+/* Private function prototypes -----------------------------------------------*/
+
+
+
+/* Private functions ---------------------------------------------------------*/
+
+void kv_map_init(kv_map_t *kv_map, const kv_str_map_t *str_map, uint32_t count)
+{
+  kv_map->str_map = str_map;
+  kv_map->count = count;
+}
+//-----------------------------------------------------------------------------
+void kv_map_kv(const kv_map_t *kv_map, char *key, char *value, jsonfl_type_t val_type, uint8_t deep) //json->cfg
+{
+  uint32_t i;
+
+  for(i = 0; i < kv_map->count; i++)
+  {
+    if(strncmp(kv_map->str_map[i].key, key, strlen(key)) == 0)
+    {
+      switch(val_type)
+      {
+        case JNFL_TYPE_STRING:
+          if(kv_map->str_map[i].type == KV_STR_TYPE_STR) strncpy((char*)kv_map->str_map[i].addr, value, kv_map->str_map[i].len-1);
+
+
+          //TODO: DOPSAT, JE POTREBA PRO ZPRACOVANI KONFIGURACE Z SMS
+
+          ((char*)kv_map->str_map[i].addr)[kv_map->str_map[i].len - 1] = 0;
+          break;
+
+        case JNFL_TYPE_NUMBER:
+          switch(kv_map->str_map[i].type)
+          {
+            case KV_STR_TYPE_UCHAR:
+              break;
+
+            case KV_STR_TYPE_CHAR:
+              break;
+
+            case KV_STR_TYPE_UINT_8:
+              *(uint8_t*)kv_map->str_map[i].addr = (uint8_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_INT_8:
+              *(int8_t*)kv_map->str_map[i].addr = (int8_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_UINT_16:
+              *(uint16_t*)kv_map->str_map[i].addr = (uint16_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_INT_16:
+              *(int16_t*)kv_map->str_map[i].addr = (int16_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_UINT_32:
+              *(uint32_t*)kv_map->str_map[i].addr = (uint32_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_INT_32:
+              *(int32_t*)kv_map->str_map[i].addr = (int32_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_UINT_64:
+              *(uint64_t*)kv_map->str_map[i].addr = (uint64_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_INT_64:
+              *(int64_t*)kv_map->str_map[i].addr = (int64_t)atoi(value);
+              break;
+
+            case KV_STR_TYPE_FLOAT:
+              *(float*)kv_map->str_map[i].addr = atof(value);
+              break;
+
+            case KV_STR_TYPE_DOUBLE:
+              *(double*)kv_map->str_map[i].addr = atof(value);
+              break;
+
+            case KV_STR_TYPE_STR:
+              strcpy((char*)kv_map->str_map[i].addr, value);
+              break;
+
+            case KV_STR_TYPE_BIN:
+              break;
+
+            case KV_STR_TYPE_OBJ:
+              break;
+
+            case KV_STR_TYPE_ARRAY:
+              break;
+
+            default:
+              break;
+          }
+
+          break;
+
+        case JNFL_TYPE_OBJECT:
+          break;
+
+        case JNFL_TYPE_ARRAY:
+          break;
+
+        case JNFL_TYPE_B_TRUE:
+          switch(kv_map->str_map[i].type)
+          {
+            case KV_STR_TYPE_UCHAR:
+              *(unsigned char*)kv_map->str_map[i].addr = '1';
+              break;
+
+            case KV_STR_TYPE_CHAR:
+              *(char*)kv_map->str_map[i].addr = '1';
+              break;
+
+            case KV_STR_TYPE_UINT_8:
+              *(uint8_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_INT_8:
+              *(int8_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_UINT_16:
+              *(uint16_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_INT_16:
+              *(int16_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_UINT_32:
+              *(uint32_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_INT_32:
+              *(int32_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_UINT_64:
+              *(uint64_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_INT_64:
+              *(int64_t*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_FLOAT:
+              *(float*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_DOUBLE:
+              *(double*)kv_map->str_map[i].addr = 1;
+              break;
+
+            case KV_STR_TYPE_STR:
+              strcpy((char*)kv_map->str_map[i].addr, "true");
+              break;
+
+            case KV_STR_TYPE_BIN:
+              break;
+
+            case KV_STR_TYPE_OBJ:
+              break;
+
+            case KV_STR_TYPE_ARRAY:
+              break;
+
+            default:
+              break;
+          }
+          break;
+
+        case JNFL_TYPE_B_FALSE:
+          switch(kv_map->str_map[i].type)
+          {
+            case KV_STR_TYPE_UCHAR:
+              *(unsigned char*)kv_map->str_map[i].addr = '0';
+              break;
+
+            case KV_STR_TYPE_CHAR:
+              *(char*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_8:
+              *(uint8_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_8:
+              *(int8_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_16:
+              *(uint16_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_16:
+              *(int16_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_32:
+              *(uint32_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_32:
+              *(int32_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_64:
+              *(uint64_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_64:
+              *(int64_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_FLOAT:
+              *(float*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_DOUBLE:
+              *(double*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_STR:
+              strcpy((char*)kv_map->str_map[i].addr, "false");
+              break;
+
+            case KV_STR_TYPE_BIN:
+              break;
+
+            case KV_STR_TYPE_OBJ:
+              break;
+
+            case KV_STR_TYPE_ARRAY:
+              break;
+
+            default:
+              break;
+          }
+          break;
+
+        case JNFL_TYPE_NULL:
+          switch(kv_map->str_map[i].type)
+          {
+            case KV_STR_TYPE_UCHAR:
+              *(unsigned char*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_CHAR:
+              *(char*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_8:
+              *(uint8_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_8:
+              *(int8_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_16:
+              *(uint16_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_16:
+              *(int16_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_32:
+              *(uint32_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_32:
+              *(int32_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_UINT_64:
+              *(uint64_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_INT_64:
+              *(int64_t*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_FLOAT:
+              *(float*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_DOUBLE:
+              *(double*)kv_map->str_map[i].addr = 0;
+              break;
+
+            case KV_STR_TYPE_STR:
+              strcpy((char*)kv_map->str_map[i].addr, "null");
+              break;
+
+            case KV_STR_TYPE_BIN:
+              break;
+
+            case KV_STR_TYPE_OBJ:
+              break;
+
+            case KV_STR_TYPE_ARRAY:
+              break;
+
+            default:
+              break;
+          }
+
+          break;
+
+        case JNFL_TYPE_UNKNOWN:
+          break;
+
+        default:
+          break;
+      }
+
+      return;
+    }
+  }
+}
+//-----------------------------------------------------------------------------
+char* kv_map_json_kv(const kv_map_t *kv_map, char *json_kv, uint32_t item) //cfg[item]->json
+{
+  //float val_f;
+
+  json_kv+=sprintf(json_kv, "\"%s\": ", kv_map->str_map[item].key);
+
+  switch(kv_map->str_map[item].type)
+  {
+    case KV_STR_TYPE_UCHAR:
+      break;
+
+    case KV_STR_TYPE_CHAR:
+      sprintf(json_kv, "\"%c\"", *(char*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_UINT_8:
+      sprintf(json_kv, "%u", *(uint8_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_INT_8:
+      sprintf(json_kv, "%i", *(int8_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_UINT_16:
+      sprintf(json_kv, "%u", *(uint16_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_INT_16:
+      sprintf(json_kv, "%i", *(int16_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_UINT_32:
+      sprintf(json_kv, "%lu", *(uint32_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_INT_32:
+      sprintf(json_kv, "%li", *(int32_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_UINT_64:
+      sprintf(json_kv, "%llu", *(uint64_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_INT_64:
+      sprintf(json_kv, "%lli", *(int64_t*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_FLOAT:
+      sprintf(json_kv, "%f", *(float*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_DOUBLE:
+      sprintf(json_kv, "%f", *(double*)kv_map->str_map[item].addr);
+      break;
+
+    case KV_STR_TYPE_STR:
+      strcat(json_kv, "\"");
+      strcat(json_kv, (char*)kv_map->str_map[item].addr);
+      strcat(json_kv, "\"");
+      break;
+
+    case KV_STR_TYPE_BIN:
+      break;
+
+    case KV_STR_TYPE_OBJ:
+      break;
+
+    case KV_STR_TYPE_ARRAY:
+      break;
+  }
+
+  //strcat(json_kv, ",\r\n");
+
+  return (json_kv + strlen(json_kv));
+}
+//-----------------------------------------------------------------------------
---
+++ kv_map.h
@@ -0,0 +1,86 @@
+/*
+ * config.h
+ *
+ *  Created on: 6. 5. 2021
+ *      Author: polasekm
+ */
+
+#ifndef KV_MAP_H_
+#define KV_MAP_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "jsonfl/jsonfl.h"
+
+
+/* Exported types ------------------------------------------------------------*/
+
+//------------------------------------------------------------------------------
+typedef enum
+{
+  KV_STR_TYPE_UCHAR        = 0,
+  KV_STR_TYPE_CHAR,
+
+  KV_STR_TYPE_UINT_8,
+  KV_STR_TYPE_INT_8,
+
+  KV_STR_TYPE_UINT_16,
+  KV_STR_TYPE_INT_16,
+
+  KV_STR_TYPE_UINT_32,
+  KV_STR_TYPE_INT_32,
+
+  KV_STR_TYPE_UINT_64,
+  KV_STR_TYPE_INT_64,
+
+  KV_STR_TYPE_FLOAT,
+  KV_STR_TYPE_DOUBLE,
+
+  KV_STR_TYPE_STR,
+  KV_STR_TYPE_BIN,
+
+  KV_STR_TYPE_OBJ,
+  KV_STR_TYPE_ARRAY
+
+} kv_str_map_type_t;
+//------------------------------------------------------------------------------
+typedef struct kv_str_map_ts kv_str_map_t;
+
+struct kv_str_map_ts
+
+{
+  char *key;                  //key name
+
+  kv_str_map_type_t type;     //value type
+  uint16_t len;               //value len (if appl.)
+
+  void (*set)(kv_str_map_t *kv_str_map, ...);      //setter fce
+  void (*get)(kv_str_map_t *kv_str_map, ...);      //getter fce
+
+  void *addr;                 //pointer to update dst. value in memory
+
+  //void (*set)(kv_str_map_t *kv_str_map);      //setter fce
+  //void (*get)(kv_str_map_t *kv_str_map);      //getter fce
+};
+
+
+typedef struct
+{
+  const kv_str_map_t *str_map;
+  uint32_t count;
+
+} kv_map_t;
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+void kv_map_init(kv_map_t *kv_map, const kv_str_map_t *str_map, uint32_t count);
+
+void kv_map_kv(const kv_map_t *kv_map, char *key, char *value, jsonfl_type_t val_type, uint8_t deep);
+char* kv_map_json_kv(const kv_map_t *kv_map, char *json_kv, uint32_t item);
+
+
+#endif /* KV_MAP_H_ */
